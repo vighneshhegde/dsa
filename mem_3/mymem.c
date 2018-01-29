ed1cbc47c710634546de35ea9efa8ef67e00c395
vighneshhegde <vighneshhegde9@gmail.com>
2018-01-29 08:38:33 +0530

    pdf

--- mem_3/mymem.c
+++ mem_3/mymem.c
@@ -5,6 +5,7 @@ size_t total_mem = 0;
 void* myalloc(size_t size){
 	total_mem+= size;
 	void* mem = malloc(size+sizeof(size_t));
+	if(mem== NULL) return mem;
 	*(size_t*)mem = size;
 	return (mem+sizeof(size_t));
 
@@ -13,4 +14,5 @@ void* myalloc(size_t size){
 void myfree(void * ptr){
 	total_mem-= *(size_t*)(ptr - sizeof(size_t)); 
 	free(ptr-sizeof(size_t));
+//	free(ptr);
 }
--- mem_3/testmymem.c
+++ mem_3/testmymem.c
@@ -1,24 +1,27 @@
 #include"mymem.h"
 #include<time.h>
+#include<unistd.h>
 
 int main(){
-	int m=100000;
+	int m=1858992000;
 	int* a;
 	srand(time(NULL));
 	do{	
-		//printf("%lu\n", total_mem);
+//		printf("%lu\n", total_mem);
 //		m = 10000+rand()%15000;
 		
 		a = (int*)myalloc(m*sizeof(int));
 		if(a==NULL){
-		printf("max memory = %lu",total_mem);
+		printf("max memory = %llu\n",total_mem);
 		break;
 		}
-//		printf("%p, %p, %d, %lu\n", a, (a+m-1), m, total_mem);
-		free(a);
-		//printf("%d\n", *a);
-		m+=10000;
-	}while(a!=NULL);
+		printf("%p, %p, %d, %llu\n", a, (a+m-1), m, total_mem);
+//		myfree(a);
+//		printf("%lu\n", total_mem);
+		m+=15000;
+	}while(a==NULL);
+//	printf("\n%d",sizeof(size_t));
+	sleep(10);
 	return 0;	
 }
 
